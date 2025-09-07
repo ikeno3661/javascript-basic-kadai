f57803e8010a852a16af4eb222b9e2e2c6e30c00
ikeno3661 <ikeno3661@gmail.com>
2025-09-08 08:12:40 +0900

    課題提出

---
+++ kadai_007/calc.js
@@ -0,0 +1,16 @@
+// numの定義
+let num = 15;
+
+// numが何の変数か出力する
+if (num%3 ==0 && num%5 == 0) {
+  console.log("3と5の倍数です")
+}
+else if (num % 5 ==0) {
+  console.log("5の倍数です");
+}
+else if (num % 3 == 0) {
+  console.log("3の倍数です");
+}
+else {
+  console.log(num)
+}
